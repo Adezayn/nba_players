c203aeee2ec8ea912c2833d26bcd73dc6869a5f3
Dolapo Adebanjo <dolapomola@gmail.com>
2022-03-06 02:45:43 +0100

    fixes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const suggestionName = document.getElementsByClassName("span_name");
 const host = "free-nba.p.rapidapi.com";
 const key = "8f7a1114f2msh2187cd8d4c81e92p156f4djsn5789829814f4";
 
+const currentYear = new Date().getFullYear();
+
 const renderError = function (error) {
   const html = `<div class="error_container"><p>${error}</p></div>`;
   nbaContainer.insertAdjacentHTML("afterbegin", html);
@@ -25,66 +27,86 @@ const renderPlayers = function (player, index) {
 //GETTING ALL THE PLAYERS FROM API
 let allPlayers, allNBAPlayers, locallySavedPlayers, allGames, allStats;
 const reformedApi = async function (route = "", param = "") {
-  const res = await fetch(`https://${host}/${route}?${param}`, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": host,
-      "x-rapidapi-key": key,
-    },
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`https://${host}/${route}?${param}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": host,
+        "x-rapidapi-key": key,
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    renderError(`💥 ${err.message}`);
+  }
 };
 const getPlayers = async function () {
   allPlayers = [];
-  for (let page = 1; page <= 38; page++) {
-    let param = `page=${page}&per_page=100`;
-    const res = await reformedApi(`players`, param);
-    allPlayers.push(...res.data);
+  try {
+    for (let page = 1; page <= 38; page++) {
+      let param = `page=${page}&per_page=100`;
+      const res = await reformedApi(`players`, param);
+      allPlayers.push(...res.data);
+    }
+  } catch (err) {
+    renderError(`💥 ${err.message}`);
   }
   return allPlayers;
 };
 
 const renderTeams = async function () {
-  const res = await fetch("https://free-nba.p.rapidapi.com/teams?page=0", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": host,
-      "x-rapidapi-key": key,
-    },
-  });
-  const data = await res.json();
-  const data2 = data.data;
-  const team = data2.map((ele) => {
-    return ele.full_name;
-  });
-  team.forEach((ele) => {
-    const html = `<option value="${ele}">${ele}</option>`;
-    teamValue.insertAdjacentHTML("beforeend", html);
-  });
+  try {
+    const res = await fetch("https://free-nba.p.rapidapi.com/teams?page=0", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": host,
+        "x-rapidapi-key": key,
+      },
+    });
+    const data = await res.json();
+    const data2 = data.data;
+    const team = data2.map((ele) => {
+      return ele.full_name;
+    });
+    team.forEach((ele) => {
+      const html = `<option value="${ele}">${ele}</option>`;
+      teamValue.insertAdjacentHTML("beforeend", html);
+    });
+  } catch (err) {
+    renderError(`💥 ${err.message}`);
+  }
 };
 //----ASYNCHRONUS IIFE -------------///
 (async function () {
-  allNBAPlayers = await getPlayers();
-  localStorage.setItem("allPlayers", JSON.stringify(allNBAPlayers));
-  renderTeams();
-})()
+  try {
+    allNBAPlayers = await getPlayers();
+    localStorage.setItem("allPlayers", JSON.stringify(allNBAPlayers));
+    renderTeams();
+  } catch (err) {
+    renderError(`💥 ${err.message}`);
+  }
+})();
 
 const nbaPlayers = async function (team) {
-  const theData = JSON.parse(localStorage.getItem("allPlayers"));
-  const data = theData
-    .map((ele) => {
-      return ele;
-    })
-    .filter((ele) => {
-      if (team === ele.team.full_name) {
+  try {
+    const theData = JSON.parse(localStorage.getItem("allPlayers"));
+    const data = theData
+      .map((ele) => {
         return ele;
-      }
-    })
-    .map((ele) => ele);
-  data.forEach((ele, index) => {
-    renderPlayers(ele, index);
-  });
+      })
+      .filter((ele) => {
+        if (team === ele.team.full_name && ele.position) {
+          return ele;
+        }
+      })
+      .map((ele) => ele);
+    data.forEach((ele, index) => {
+      renderPlayers(ele, index);
+    });
+  } catch (err) {
+    renderError(`💥 ${err.message}`);
+  }
 };
 
 ///EXECUTION
@@ -94,34 +116,41 @@ teamValue.addEventListener("change", function () {
   nbaPlayers(theValue);
 });
 
-const currentYear = new Date().getFullYear();
 //----------GETTING THE TOP SCORERS----------///
 const nbaGames = async function () {
   allGames = [];
-  for (let page = 1; page <= 14; page++) {
-    let param = `page=${page}&per_page=100&seasons[]=${currentYear - 1}&seasons[]=${currentYear}`;
-    const res = await reformedApi(`games`, param);
-    allGames.push(...res.data);
-  }
-  const getLatestDate = allGames
-    .sort((a, b) => {
-      return a.date - b.date;
-    })
-    .slice(-3);
+  try {
+    for (let page = 1; page <= 14; page++) {
+      let param = `page=${page}&per_page=100&seasons[]=${
+        currentYear - 1
+      }&seasons[]=${currentYear}`;
+      const res = await reformedApi(`games`, param);
+      allGames.push(...res.data);
+    }
+    const getLatestDate = allGames
+      .sort((a, b) => {
+        return a.date - b.date;
+      })
+      .slice(-3);
 
-  getLatestDate.forEach(async (game) => {
-    let param = `per_page=100&game_ids[]=${game.id}`;
-    const res = await reformedApi(`stats`, param);
-    const [getTopPts] = res.data.sort((a, b) => b.pts - a.pts).slice(0, 1);
-    const html = `<div class="top_grid">
+    getLatestDate.forEach(async (game) => {
+      let param = `per_page=100&game_ids[]=${game.id}`;
+      const res = await reformedApi(`stats`, param);
+      const [getTopPts] = res.data.sort((a, b) => b.pts - a.pts).slice(0, 1);
+      const html = `<div class="top_grid">
     <p class="top_scorers_date">${game.date.split("T")[0]}</p>
-    <p class="top_scorers_name">${getTopPts.player.first_name} ${getTopPts.player.last_name}</p>
+    <p class="top_scorers_name">${getTopPts.player.first_name} ${
+        getTopPts.player.last_name
+      }</p>
     <p class="top_scorers_team">${getTopPts.team.full_name}</p>
     <p class="top_scorers_position">${getTopPts.player.position}</p>
     <p class="top_scorers_pts">${getTopPts.pts}</p>
   </div>`;
-    topScorerContainer.insertAdjacentHTML("beforeend", html);
-  });
+      topScorerContainer.insertAdjacentHTML("beforeend", html);
+    });
+  } catch (err) {
+    renderError(`💥 ${err.message}`);
+  }
 };
 
 nbaGames();
